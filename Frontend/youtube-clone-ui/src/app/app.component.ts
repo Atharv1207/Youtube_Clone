d093e0f457de6031d7461385d13ebd3d987820ad
atharv_mangal_ <atharv.mangal@sjsu.edu>
2024-01-08 16:32:05 -0800

    saved-videos component

--- Frontend/youtube-clone-ui/src/app/app.component.ts
+++ Frontend/youtube-clone-ui/src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {FormsModule} from "@angular/forms";
+import {FormControl, FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {NgxFileDropModule} from "ngx-file-drop";
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
 import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {MatOptionModule} from "@angular/material/core";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormsModule, HttpClientModule, NgxFileDropModule, HeaderComponent, FlexLayoutModule],
+  imports: [CommonModule, RouterOutlet, FormsModule, HttpClientModule, NgxFileDropModule, HeaderComponent, FlexLayoutModule, MatFormFieldModule, MatSelectModule, MatOptionModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   styleUrl: './app.component.css'
--- Frontend/youtube-clone-ui/src/app/save-video-details/save-video-details.component.ts
+++ Frontend/youtube-clone-ui/src/app/save-video-details/save-video-details.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import {FlexModule} from "@angular/flex-layout";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {FormControl, FormGroup} from "@angular/forms";
+import{ReactiveFormsModule as NgModule} from "@angular/forms";
 
 @Component({
   selector: 'app-save-video-details',
   standalone: true,
-  imports: [],
+  imports: [
+    FlexModule,
+    MatFormFieldModule,
+    MatSelectModule
+  ],
   templateUrl: './save-video-details.component.html',
   styleUrl: './save-video-details.component.css'
 })
 export class SaveVideoDetailsComponent {
+
+  title: FormControl = new FormControl('');
+  description: FormControl = new FormControl('')
+  videoStatus: FormControl = new FormControl('')
+  saveVideoDetailsForm: FormGroup;
+
+  constructor() {
+    this.saveVideoDetailsForm = new FormGroup({
+      title: this.title,
+      description: this.description,
+      vidoeStatus: this.videoStatus
+    })
+
+  }
 
 }
